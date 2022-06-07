218d4fbc049df9467c7e32aa9a5d09b02ab40a75
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-06-07 10:35:15 +0530

    Protecting Systems with Circuit Breakers

---
+++ 2.protecting-microsercices-circuit-breaker/fastpass-ui/src/main/java/com/example/demo/FastPassCustomer.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+public class FastPassCustomer {
+    private String fastPassId;
+    private String customerFullName;
+    private String customerPhone;
+    private Float currentBalance;
+}
---
+++ 2.protecting-microsercices-circuit-breaker/tollrate-ui/src/main/java/com/example/demo/DashboardController.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreaker;
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import reactor.core.publisher.Mono;
+
+@Controller
+public class DashboardController {
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+    
+    @Autowired
+    private ReactiveCircuitBreakerFactory circuitBreakerFactory;
+
+    @RequestMapping("/dashboard")
+    public String getTollRate(@RequestParam(defaultValue = "1000") Integer stationId, Model m) {
+    	
+    	ReactiveCircuitBreaker rcb = circuitBreakerFactory.create("tollrate-cb");
+    	
+    	Mono<TollRate> rate = rcb.run(webClientBuilder.build()
+                .get()
+                .uri("http://tollrate-service/tollrateslow/" + stationId)
+                .retrieve()
+                .bodyToMono(TollRate.class), throwable -> getDefaultRate());
+
+
+
+        System.out.println("stationId: " + stationId);
+        m.addAttribute("rate", rate.block());
+        return "dashboard";
+    }
+    
+    
+    private Mono<TollRate> getDefaultRate(){
+    	System.out.println("Fallback method called ---");
+    	return Mono.just(new TollRate(0, 0.00f, "Dummy"));
+    }
+}
