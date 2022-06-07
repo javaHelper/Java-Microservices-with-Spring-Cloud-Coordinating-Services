f5c158231ff19c5d10a03e51ad45fafb391a958f
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-06-07 18:05:48 +0530

    Changing Load Balancer Behavior Through Configurations

---
+++ 4.changing-loadbalancer-behaviour-through-configurations/fastpass-ui/src/main/java/com/example/FastpassUiApplication.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@LoadBalancerClient(name = "fastpass-service", configuration = LoadBalancerAlgorithmConfig.class)
+@SpringBootApplication
+public class FastpassUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastpassUiApplication.class, args);
+	}
+
+	@Bean
+	@LoadBalanced
+	public WebClient.Builder loadBalancedWebClientBuilder() {
+		return WebClient.builder();
+	}
+}
