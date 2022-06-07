a4d343bf40df8a3878cdf2dde6c5e5a39db349ee
Prateek <54174687+javaHelper@users.noreply.github.com>
2022-06-07 17:19:01 +0530

    Customizing Load Balancer Through Configurations

---
+++ 3.customizing-load-balancer-Eureka-work-together/fastpass-service/src/main/java/com/example/FastpassServiceApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastpassServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastpassServiceApplication.class, args);
+	}
+}
